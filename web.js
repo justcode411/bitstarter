487c0f61ae097d565ae8ff33842d69f72fd21959
Paul R. Rondeau <justcode@gmail.com>
2013-07-06 22:22:04 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, response) 
+{
+    var s = fs.readFileSync(infile, 'utf8');
+    response.send( s.toString() ); 
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
